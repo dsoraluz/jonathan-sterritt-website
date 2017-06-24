73ce4a38db4359d2cccfe6c76be317e78773f305
dsoraluz <d.soraluz@gmail.com>
2017-06-24 19:18:20 -0400

    styling added to sidenav

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -23,7 +23,13 @@ $(document).ready(()=>{
       });
     });
 
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+      menuWidth: 1000, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    }
+  );
 
   $(".side-nav li>a").click(()=>{
     $('.button-collapse').sideNav('hide');
