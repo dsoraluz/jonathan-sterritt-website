f94f8490bb10e2ba7b97cc8b4b58f55b5a537e84
Daniel Soraluz <dsoraluz@carecloud.com>
2018-02-20 19:58:36 -0500

    added new email settings to nodemailer, responsive text for contact section, fixed broken brutus link, added new styles to contact

--- routes/mail.js
+++ routes/mail.js
@@ -4,32 +4,34 @@ const nodemailer = require('nodemailer');
 const mailRouter = express.Router();
 
 mailRouter.post('/contact', (req, res, next)=>{
+  
+  let name = req.body.name;
+  
+  let email = req.body.email;
+  
+  let message = req.body.message;
 
-let name = req.body.name;
-
-let email = req.body.email;
-
-let message = req.body.message;
-
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GMAIL_USERNAME,
-    pass: process.env.GMAIL_PASSWORD
-  }
-});
-
-let mailOptions = {
-  from: `A new message from ${name}<${email}>`,
-  to: `${process.env.GMAIL_USERNAME}`,
-  subject: `Someone is looking for Jonathan Sterritt.`,
-  html: `${message}`
-};
-
-transporter.sendMail(mailOptions, (err, info)=>{
-    if (error){
-      res.send(500);
-      console.log(error);
+      
+  let transporter = nodemailer.createTransport({
+    host: 'gator4138.hostgator.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  
+  let mailOptions = {
+    from: `A new message from ${name}<${email}>`,
+    to: `${process.env.EMAIL_USERNAME}`,
+    subject: `Someone is looking for Jonathan Sterritt.`,
+    html: `${message}`
+  };
+  transporter.sendMail(mailOptions, (err, info)=>{
+    if (err){
+      // res.sendStatus(500);
+      console.log(err);
       return;
     } else {
       console.log('Message %s sent: %s', info.messageId, info.response);
